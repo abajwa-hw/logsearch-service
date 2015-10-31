12925492a99c30eb433a5eedab6d3d8673aaaf38
Ali Bajwa <abajwa@hortonworks.com>
2015-10-30 19:11:07 -0700

    updated port to 8886

--- package/scripts/status_params.py
+++ package/scripts/status_params.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 from resource_management import *
+import glob
 
 # config object that holds the status related configurations declared in the -env.xml file
 config = Script.get_config()
 
 #solr pid file
 solr_piddir = config['configurations']['solr-env']['stack_piddir']
-solr_pidfile = format("{solr_piddir}/solr-{solr_port}.pid")
+solr_pidfile = format("{solr_piddir}/solr-8886.pid")
+
 
 #logsearch pid file
 logsearch_pid_dir = config['configurations']['logsearch-env']['logsearch_pid_dir']
