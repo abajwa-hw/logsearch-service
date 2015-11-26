1d58355b2fc8f02e8ea08e35bdd7d9529765bc29
Don Bosco Durai <bosco@apache.org>
2015-11-25 16:34:16 -0800

    disabled JMX for now

--- package/scripts/start_logfeeder.sh
+++ package/scripts/start_logfeeder.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
-
+if [ $# -ne 4 ]; then
+    echo "Error: Not enough parameters. Count=$# excpected 4"
+    echo "Usage: <logsearch path> <logfile> <pid file> <java_home>"
+    echo "Example: $0 /opt/logfeeder /var/log/logfeeder/logfeeder.log /var/run/logfeeder/logfeeder.pid /usr/jdk64/jdk1.8.0_45"
+    exit 1
+fi
 
 #path containing logfeeder e.g. /opt/logfeeder
 LOGSEARCH_PATH=$1
@@ -13,10 +18,30 @@ PID_FILE=$3
 
 JAVA_HOME=$4
 
-LOGFEEDER_JAVA_MEM="-Xmx512m"
-#Temporarily enabling JMX so we can monitor the memory and CPU utilization of the process
-JMX="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=2098"
+if [ -f ${PID_FILE} ]; then
+    PID=`cat ${PID_FILE}`
+    if kill -0 $PID 2>/dev/null; then
+	echo "logfeeder already running (${PID}) killing..."
+	kill $PID 2>/dev/null
+	sleep 5
+	if kill -0 $PID 2>/dev/null; then
+	    echo "logfeeder still running. Will kill process forcefully in another 10 seconds..."
+	    sleep 10
+	    kill -9 $PID 2>/dev/null
+	    sleep 2
+	fi
+    fi
+    if kill -0 $PID 2>/dev/null; then
+	echo "ERROR: Even after all efforts to stop logfeeder, it is still running. pid=$PID. Please manually kill the service and try again."
+	exit 1
+    fi
+fi
 
+
+LOGFEEDER_JAVA_MEM="-Xmx512m"
+#JMX="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=2098"
+
+#Incase you want to send additional jar or configs to the logfeeder when ran as offline
 #LOGFEEDER_CLI_CLASSPATH=
  
 cd $LOGSEARCH_PATH
